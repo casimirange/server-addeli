90e4b679a2e4b1660331cc002a8e2fb3d020fd2a
casimirange <angecasimir41@gmail.com>
2023-08-15 16:02:17 +0100

    update

--- src/main/java/com/example/demo/ServeurTpmApplication.java
+++ src/main/java/com/example/demo/ServeurTpmApplication.java
@@ -5,17 +5,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 public class ServeurTpmApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ServeurTpmApplication .class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ServeurTpmApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ServeurTpmApplication .class);
+	@Bean
+	public ViewResolver viewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/classes/");
+		resolver.setSuffix(".jsp");
+		resolver.setExposeContextBeansAsAttributes(true);
+		return resolver;
 	}
 
 }
